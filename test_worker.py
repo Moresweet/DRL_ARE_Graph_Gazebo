a64fc528b9f9576a04eb37dfb5b6f0156a6eee8a
Moresweet <tianchanghang@163.com>
2024-01-01 17:51:03 +0800

    20240101 修改时间度量

--- test_worker.py
+++ test_worker.py
@@ -72,12 +72,15 @@ class TestWorker:
 
             policy_center_frontier = self.select_closest_frontier(next_position)
 
-            start_time = rospy.Time.now()
+            # start_time = rospy.Time.now()
+            start_time = timeit.default_timer()
             reward, done, self.robot_position, self.travel_dist, plan_status, no_nbv, object_reward = self.env.step(
                 policy_center_frontier,
                 next_position,
                 self.travel_dist)
-            end_time = rospy.Time.now()
+            # 注意，不要使用rostime，因为gazebo的仿真加速，导致这个时间其实并不可靠
+            # end_time = rospy.Time.now()
+            end_time = timeit.default_timer()
             time_elapsed = end_time - start_time
             time_elapsed_sec = time_elapsed.to_sec()
             self.exploration_time.append(time_elapsed_sec)
